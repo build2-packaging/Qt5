fc6de87a45cc905a1ed798cc01f5892944df3f5f
Francois Kritzinger <francois@codesynthesis.com>
2021-11-29 10:12:36 +0200

    qconfig.cpp: Support installed and uninstalled paths

---
+++ libQt5Core/QtCore/global/qconfig.cpp
@@ -0,0 +1,12 @@
+// @@ This file is included by corelib/global/qlibraryinfo.cpp only (never
+//    compiled). See the buildfile and then qconfig.cpp.in for details.
+//
+
+extern const char qt_configure_prefix_path_str[12 + 256];
+#define QT_CONFIGURE_PREFIX_PATH qt_configure_prefix_path_str + 12
+
+extern const short qt_configure_str_offsets[13];
+extern const char* qt_configure_strs;
+
+extern const char* QT_CONFIGURE_SETTINGS_PATH;
+extern const char* QT_CONFIGURE_LIBLOCATION_TO_PREFIX_PATH;
