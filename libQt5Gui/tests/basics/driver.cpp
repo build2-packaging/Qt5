a68c01209deab79c2c06141818f84e20cdf048f5
Francois Kritzinger <francois@codesynthesis.com>
2022-11-17 16:08:57 +0200

    Build libQt5Gui

--- libQt5Gui/tests/basics/driver.cpp
+++ libQt5Gui/tests/basics/driver.cpp
@@ -1,7 +1,13 @@
-#include <QtGui/XXX>
+// The QGuiApplication constructor should load at least the platform
+// plugin. Set the environment variable QT_DEBUG_PLUGINS to 1 for
+// confirmation.
+//
+
+#include <QtGui/QGuiApplication>
 
 int
-main ()
+main (int argc, char** argv)
 {
+  QGuiApplication app (argc, argv);
   return 0;
 }
